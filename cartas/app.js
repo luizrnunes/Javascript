fdcf3be1289a88831c54e08898760e56158aabeb
luizrnunes <luiz.hrnunes@senacsp.edu.br>
2025-12-03 14:15:28 -0300

    att

--- cartas/app.js
+++ cartas/app.js
@@ -1,14 +1,14 @@
 /**
  * Sorteio de uma carta
  * Exemplo de uso de array para otimizar o código
- * @author Luiz Hnerique Rodrigues Nunes
+ * @author Luiz Henrique Rodrigues Nunes
  */
 
-function sortear(){
+function sortear() {
     let nipes = ["♥", "♦", "♣", "♠"]
     let faces = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
-    //Apoio a lógica
+    //apoio a lógica
     //console.log(faces[10])
     //console.log(nipes[3])
 
@@ -21,13 +21,37 @@ function sortear(){
     //console.log(face)
     //console.log(nipe)
 
-    //renderização do canto superior esquerdo da carta
-    //.innerHTML insere uma tag no documento HTML
-    document.getElementById('supEsq').innerHTML = `<div>${face}</div> <div>${nipe}</div>`
-    
-    //renderização do centro da carta
-    document.getElementById('centroCarta').innerHTML = `<div>${nipe}</div>`
+    //Determinar a cor com base no nipe sorteado
+    let cor
+    if (nipe === "♥" || nipe === "♦") {
+        cor = "#ff0000"
+    } else {
+        cor = "#000"
+    }
 
     //renderização do canto superior esquerdo da carta
+    //.innerHTML insere uma tag no documento html
+    document.getElementById('supEsq').innerHTML = `<div>${face}</div> <div>${nipe}</div>`
+    //mudar a cor (O JS consegue também manipular o CSS usando .style)
+    document.getElementById('supEsq').style.color = cor
+
+    //renderização do centro da carta
+    let cc = document.getElementById('centroCarta')
+    if (face === 'J') {
+        cc.innerHTML = `<img src="./img/valete.png">`
+    } else if (face === 'Q') {
+        cc.innerHTML = `<img src="./img/dama.png">`
+    } else if (face === 'K') {
+        cc.innerHTML = `<img src="./img/rei.png">`
+    } else {
+        cc.innerHTML = nipe
+        cc.style.color = cor
+    }
+
+
+    //renderização do canto inferior direito da carta   
     document.getElementById('infDir').innerHTML = `<div>${face}</div> <div>${nipe}</div>`
+    //mudar a cor
+    document.getElementById('infDir').style.color = cor
+
 }
